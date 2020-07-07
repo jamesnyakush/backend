6c86d79ccb546a0ad804b02a1b0233bf06d93b6b
jamesnyakush <emvesttech@gmail.com>
2020-07-08 01:49:22 +0300

    Added docs :rocket:

--- internal/store/model/bill.go
+++ internal/store/model/bill.go
@@ -1,9 +1,12 @@
 package model
 
-import "time"
+import (
+	"github.com/gofrs/uuid"
+	"time"
+)
 
 type Bill struct {
-	BillId                uint      `json:"bill_id"`
+	BillId                uuid.UUID `json:"bill_id"`
 	UserId                uint      `json:"user_id"`
 	BillingDate           time.Time `json:"billing_date"`
 	LastBillingDate       time.Time `json:"last_billing_date"`
