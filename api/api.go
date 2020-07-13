3cc597a22c5fe47782af8722417a5795075c49fe
jamesnyakush <emvesttech@gmail.com>
2020-07-13 18:25:21 +0300

    Added to api.go and server.go added CORS :rocket:

--- api/api.go
+++ api/api.go
@@ -1,1 +1,26 @@
 package api
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/nyumbapoa/backend/internal/config"
+)
+
+func New(enableCORS bool, cfg *config.Config) (*chi.Mux, error) {
+
+	r := chi.NewRouter()
+	return r, nil
+}
+
+func corsConfig() *cors.Cors {
+	return cors.New(cors.Options{
+
+		AllowedOrigins:     []string{"*"},
+		AllowedMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:     []string{"Accept", "Authorization", "Accept-Encoding", "Content-Type", "Content-Length", "X-CSRF-Token"},
+		ExposedHeaders:     []string{"Link"},
+		AllowCredentials:   true,
+		MaxAge:             86400,
+		OptionsPassthrough: false,
+	})
+}
